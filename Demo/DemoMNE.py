f018ed5c904dbbe603aba9a7721b59b6c30396c4
Nathan-Draudt <78226179+Nathan-Draudt@users.noreply.github.com>
2022-04-18 15:51:24 -0400

    Create DemoMNE.py

---
+++ Demo/DemoMNE.py
@@ -0,0 +1,9 @@
+from cProfile import run
+from pandas import ExcelFile
+
+
+##executes a file that shows effectivly that this version of the system works in this environment. Will download some sample files if you do not give it imputs
+#exec(open('plot_20_enhance.py').read())
+import plot_20_enhance
+plot_20_enhance
+
